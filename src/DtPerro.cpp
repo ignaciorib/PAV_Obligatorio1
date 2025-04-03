ecdcbe03cda17d68f5c2aecd549501f52920bb3d
adele1805 <adeleon1805@hotmail.com>
2025-04-03 16:01:06 -0300

    Update DtPerro.cpp

--- src/DtPerro.cpp
+++ src/DtPerro.cpp
@@ -11,7 +11,29 @@
 
 DtPerro::DtPerro() {}
 
-DtPerro::DtPerro(RazaPerro r, bool vC) {
-    raza = r;
+DtPerro::DtPerro(int r, bool vC) {
+    switch (r) {
+        case 1:
+            raza = RazaPerro::labrador;
+            break;
+        case 2:
+            raza = RazaPerro::ovejero;
+            break;
+        case 3:
+            raza = RazaPerro::bulldog;
+            break;
+        case 4:
+            raza = RazaPerro::pitbull;
+            break;
+        case 5:
+            raza = RazaPerro::collie;
+            break;
+        case 6:
+            raza = RazaPerro::pekines;
+            break;
+        case 7:
+            raza = RazaPerro::otro;
+            break;
+    }
     vacunaCachorro = vC;
 }
