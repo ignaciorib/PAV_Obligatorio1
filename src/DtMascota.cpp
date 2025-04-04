73cea7cdb398b0cffc0134726daf3527b0301316
adele1805 <adeleon1805@hotmail.com>
2025-04-04 00:51:00 -0300

    Update DtMascota.cpp

--- src/DtMascota.cpp
+++ src/DtMascota.cpp
@@ -3,20 +3,24 @@
 #include <iostream>
 #include <string.h>
 
-#include "../headers/Genero.h"
-#include "../headers/DtMascota.h"
+#include "Genero.h"
+#include "DtMascota.h"
 
 
 DtMascota::DtMascota() {}
 
-DtMascota::DtMascota(string n, int g, float p, float rD) {
+DtMascota::DtMascota(string n, int g, float p, int p_g) {                       //1 es gato, 0 es perro
     nombre = n;
-    if g
+    if (g)  
         genero = Genero::Macho;
-    else
-        genero = Genero::Hambra;
+    else 
+        genero = Genero::Hembra;
     peso = p;
-    racionDiaria = rD;
+    P_G = p_g;
+    if (p_g)
+        racionDiaria = p * 0,015;
+    else
+        racionDiaria = p * 0,025;
 }
 
 void DtMascota::setNombre(string DtNombre){
@@ -32,4 +36,27 @@ void DtMascota::setGenero(int DtGenero){
 
 void DtMascota::setPeso(float DtPeso){
     peso = DtPeso;
+}
+
+void DtMascota::setPerroGato(int tipoMascot){
+    P_G = tipoMascot;
+}
+
+int DtMascota::getPerroGato() {
+    return P_G;
+}
+
+int DtMascota::getDtGenero() {
+    if (genero == Genero::Macho)
+        return 1;
+    else
+        return 0;
+}
+
+float DtMascota::getDtPeso() {
+    return peso;
+}
+
+string DtMascota::getNombreDtMsc(){
+    return nombre;
 }
