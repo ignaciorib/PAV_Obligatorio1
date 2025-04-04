a43fe7243bc750b8711d0570242318a607dcb6b9
adele1805 <adeleon1805@hotmail.com>
2025-04-04 15:29:24 -0300

    Update Consulta.h

--- headers/Consulta.h
+++ headers/Consulta.h
@@ -1,16 +1,21 @@
 #ifndef CONSULTA_H
 #define CONSULTA_H
 
-#include <string>
+#include <iostream>
+#include <string.h>
+
+#include "DtFecha.h"
 
 using namespace std;
+
 
 class Consulta {
     private:
         string descripcion;
+        DtFecha fechaIngreso;
     public:
         Consulta();
-        Consulta(string);
+        Consulta(string, int, int, int);
         //void mostrarInfo();
 };
 
